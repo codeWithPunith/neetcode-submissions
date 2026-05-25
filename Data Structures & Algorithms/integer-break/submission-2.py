3359b8c798e8345bbe7639508564d3b48ff47b16
Punithraj R <codewithpunith@gmail.com>
2026-05-25 23:14:13 +0530

    Add: integer-break - submission-2

---
+++ Data Structures & Algorithms/integer-break/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp={1:1}
+
+        for num in range(2,n+1):
+            dp[num] = 0 if num==n else num
+            for i in range(1,num):
+                val = dp[i]*dp[num-i]
+                dp[num] = max(val,dp[num])
+        return dp[n]
+
+
+        def dfs(num):
+            if num in dp:
+                return dp[num]
+            dp[num] =0 if num==n else num
+            for i in range(1,num):
+                val= dfs(i)*dfs(num-i)
+                dp[num] =max(val,dp[num])
+            return dp[num]
+       # return dfs(n)
+
+        
